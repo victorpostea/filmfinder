1770edf7ad786376905dc1256694635cde59c5c8
victorpostea <victorpostea@gmail.com>
2025-03-06 13:20:17 -0500

    header

--- movie-frontend/src/app/app-routing.module.ts
+++ movie-frontend/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { SearchComponent } from './search/search.component';
+import { RecommendComponent } from './recommend/recommend.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'search', component: SearchComponent },
+  { path: 'recommend', component: RecommendComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' }, // Default route
+  { path: '**', redirectTo: '/home' } // Wildcard route to redirect unknown paths
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
