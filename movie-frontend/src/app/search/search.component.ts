45b4ed641840d0d390e25457ea8361973d4b1496
victorpostea <vpostea@uwo.ca>
2025-03-06 23:36:06 -0500

    added some defaults for data missing

--- movie-frontend/src/app/search/search.component.ts
+++ movie-frontend/src/app/search/search.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { MovieService } from '../movie.service';
 import { Observable } from 'rxjs';
 import { Movie, MovieResponse } from '../models/movie.model';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-search',
@@ -13,17 +14,41 @@ export class SearchComponent {
   queryParam: string = '';
   movie_list: Observable<MovieResponse> | null = null;
 
-  constructor(private movieservice: MovieService){}
+  constructor(private movieservice: MovieService) {}
 
   submitInput() {
     console.log("user input submitted: ", this.userInput);
-    this.queryParam = this.userInput.replace(/ /g, "&20")
+    // Replace spaces with %20
+    this.queryParam = this.userInput.replace(/ /g, "%20");
     this.findFilms();
   }
 
   findFilms() {
     if (this.userInput) {
-      this.movie_list = this.movieservice.searchMovie(this.queryParam);
+      this.movie_list = this.movieservice.searchMovie(this.queryParam).pipe(
+        map(response => this.parseData(response))
+      );
     }
+  }
+
+  parseData(response: MovieResponse): MovieResponse {
+    if (!response.results) {
+      response.results = [];
+      return response;
+    }
+    
+    response.results = response.results.map(movie => ({
+      ...movie,
+      title: movie.title || "Unknown Title",
+      release_date: movie.release_date || "N/A",
+      vote_average: typeof movie.vote_average === "number" ? movie.vote_average : 0,
+      overview: movie.overview || "No overview available.",
+      // Build full poster URL or use fallback if not available
+      poster_path: movie.poster_path 
+        ? `https://image.tmdb.org/t/p/w500${movie.poster_path}` 
+        : "/assets/img_gone.png"
+    }));
+  
+    return response;
   }
 }
